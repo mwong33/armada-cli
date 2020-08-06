1276b94f46ea3871a51f940473c014066567e8a3
Matthew James Wong <matthew.wong76@gmail.com>
2020-08-05 18:07:29 -0600

    Finishing basic command instructions for ArmadaDiceSimulator

--- ArmadaDiceSimulator.java
+++ ArmadaDiceSimulator.java
@@ -10,7 +10,8 @@ public class ArmadaDiceSimulator {
         // Introduction prompt
         System.out.println("Welcome to Armada Dice Simulator!");
         System.out.println("Let's create your dice pool");
-        
+        System.out.println();
+
         // Setting up the dice pool
         System.out.println("How many RED dice are in your pool? Enter as an Integer");
 
@@ -19,6 +20,7 @@ public class ArmadaDiceSimulator {
             redDiceCount = in.nextInt();
         }
 
+        System.out.println();
         System.out.println("How many BLUE dice are in your pool? Enter as an Integer");
 
         int blueDiceCount = 0;
@@ -26,6 +28,7 @@ public class ArmadaDiceSimulator {
             blueDiceCount = in.nextInt();
         }
 
+        System.out.println();
         System.out.println("How many BLACK dice are in your pool? Enter as an Integer");
 
         int blackDiceCount = 0;
@@ -34,7 +37,46 @@ public class ArmadaDiceSimulator {
         }
 
         // Create the Dice Pool
-       
+        Dice[] dicePoolArray = createDicePoolArray(redDiceCount, blueDiceCount, blackDiceCount);
+
+        DiceSim theDiceSim = new DiceSim(dicePoolArray);
+
+        // Give Crit Stats
+        System.out.println();
+        System.out.println("You created a dice pool with " + redDiceCount + " red die, " + blueDiceCount + " blue die, and " + blackDiceCount + " black dice");
+        System.out.println();
+        System.out.println("Here are the chance to crit stats of your dice pool: ");
+        theDiceSim.displayChanceToCrit();
+
+        // Give the instructions to simulate and run the simulation
+        boolean continueSim = true;
+
+        while (continueSim) {
+            System.out.println();
+            System.out.println("Enter an Integer to simulate your dice pool");
+            System.out.println("Quit the simulation at any time by typing 'quit'");
+            System.out.println("To reset the simulation type 'reset'");
+            System.out.println();
+            
+            if (in.hasNextInt()) {
+                int simCount = in.nextInt();
+                System.out.println();
+                System.out.println("Simulation Results:");
+                theDiceSim.simulate(simCount);
+                theDiceSim.printStats();
+            } else if (in.hasNext()) {
+                String command = in.next();
+                if (command.equals("quit")) {
+                    continueSim = false;
+                } else if (command.equals("reset")) {
+                    theDiceSim.reset();
+                    System.out.println();
+                    System.out.println("Simulation has been reset:");
+                    theDiceSim.printStats();
+                }
+            }
+        }
+
         in.close();
         
     }
@@ -46,7 +88,7 @@ public class ArmadaDiceSimulator {
      * @param blackCount - The number of black dice int he returned Dice array.
      * @return A Dice array with the given number of red, blue and black dice according to the provided parameters.
      */
-    private static Dice[] createDicePool(int redDiceCount, int blueDiceCount, int blackDiceCount) {
+    private static Dice[] createDicePoolArray(int redDiceCount, int blueDiceCount, int blackDiceCount) {
         
         Dice[] theDiceArray = new Dice[redDiceCount + blueDiceCount + blackDiceCount];
 
